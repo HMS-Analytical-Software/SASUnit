8e3c8f7652a701ab3326f60bee8320fb62c76b2b
klandwich <klandwich@eff360d0-8e89-467c-86ba-6985a6ba51a3>
2012-10-24 14:56:46 +0000

    Added Support for different Versions of SAS Commit is related to the following issues: 83

---
+++ bin/sasunit.9.3_32.windows.de.cmd
@@ -0,0 +1,7 @@
+cd ..
+SET SASUNIT_ROOT=.\
+SET SASUNIT_OVERWRITE=0
+SET SASUNIT_LANGUAGE=de
+SET SASUNIT_HOST_OS=windows
+SET SASUNIT_SAS_VERSION=9.3_32
+"C:\Program Files\x86\SASHome\SASFoundation\9.3\sas.exe" -CONFIG "bin\sasunit.%SASUNIT_SAS_VERSION%.%SASUNIT_HOST_OS%.%SASUNIT_LANGUAGE%.cfg" -no$syntaxcheck -noovp -nosplash
---
+++ bin/sasunit.9.3_32.windows.de.overwrite.cmd
@@ -0,0 +1,7 @@
+cd ..
+SET SASUNIT_ROOT=.\
+SET SASUNIT_OVERWRITE=1
+SET SASUNIT_LANGUAGE=de
+SET SASUNIT_HOST_OS=windows
+SET SASUNIT_SAS_VERSION=9.3_32
+"C:\Program Files\SASHome\x86\SASFoundation\9.3\sas.exe" -CONFIG "bin\sasunit.%SASUNIT_SAS_VERSION%.%SASUNIT_HOST_OS%.%SASUNIT_LANGUAGE%.cfg" -no$syntaxcheck -noovp -nosplash
