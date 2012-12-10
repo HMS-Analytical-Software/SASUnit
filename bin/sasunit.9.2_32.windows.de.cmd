386af29b23de10dea8fe43e3b8308623c5d132c6
klandwich <klandwich@eff360d0-8e89-467c-86ba-6985a6ba51a3>
2012-12-10 11:39:51 +0000

    Included a check for the return code of the SAS session. If unequal zero then the return code is printed in the console. Commit is related to the following issues: 72

--- bin/sasunit.9.2_32.windows.de.cmd
+++ bin/sasunit.9.2_32.windows.de.cmd
@@ -5,3 +5,11 @@ SET SASUNIT_LANGUAGE=de
 SET SASUNIT_HOST_OS=windows
 SET SASUNIT_SAS_VERSION=9.2_32
 "C:\Program Files\SAS\SASFoundation\9.2(32-bit)\sas.exe" -CONFIG "bin\sasunit.%SASUNIT_SAS_VERSION%.%SASUNIT_HOST_OS%.%SASUNIT_LANGUAGE%.cfg" -no$syntaxcheck -noovp -nosplash
+
+@echo off
+if %ERRORLEVEL%==0 goto normalexit
+@echo. 
+@echo Exit code: %ERRORLEVEL%
+@echo. 
+pause
+:normalexit
--- bin/sasunit.9.3.windows.de.overwrite.cmd
+++ bin/sasunit.9.3.windows.de.overwrite.cmd
@@ -5,3 +5,11 @@ SET SASUNIT_LANGUAGE=de
 SET SASUNIT_HOST_OS=windows
 SET SASUNIT_SAS_VERSION=9.3
 "C:\Program Files\SASHome\SASFoundation\9.3\sas.exe" -CONFIG "bin\sasunit.%SASUNIT_SAS_VERSION%.%SASUNIT_HOST_OS%.%SASUNIT_LANGUAGE%.cfg" -no$syntaxcheck -noovp -nosplash
+
+@echo off
+if %ERRORLEVEL%==0 goto normalexit
+@echo. 
+@echo Exit code: %ERRORLEVEL%
+@echo. 
+pause
+:normalexit
