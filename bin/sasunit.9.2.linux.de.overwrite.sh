ab679d58e7e2d22dc02b3c774ca0044822f85da6
klandwich <klandwich@eff360d0-8e89-467c-86ba-6985a6ba51a3>
2012-12-19 06:29:07 +0000

    Reworked the linux scripts with the suggestion from Nikolas Aggelidakis to ask for opening of the created documentation Commit is related to the following issues: 74

--- bin/sasunit.9.2.linux.de.overwrite.sh
+++ bin/sasunit.9.2.linux.de.overwrite.sh
@@ -6,4 +6,12 @@ export SASUNIT_LANGUAGE=de
 export SASUNIT_HOST_OS=linux
 export SASUNIT_SAS_VERSION=9.2
 export SASCFGPATH=./bin/sasunit.$SASUNIT_SAS_VERSION.$SASUNIT_HOST_OS.$SASUNIT_LANGUAGE.cfg
+echo "Starting SASUnit in Overwrite Mode ..."
 /usr/local/SAS/SASFoundation/$SASUNIT_SAS_VERSION/bin/sas_$SASUNIT_LANGUAGE -nosyntaxcheck -noovp
+echo "SAS Exit Status: $?"          # Show SAS exit status
+if [ -f  doc/sasunit/$SASUNIT_LANGUAGE/rep/index.html ]          
+  then read -p "Would you like to see the results [y|n]? "
+  case "$REPLY" in
+   y|Y) xdg-open doc/sasunit/$SASUNIT_LANGUAGE/rep/index.html;;
+  esac
+fi
