db0c9c71b0a29abe6352ab42bccc5355972c8aa2
klandwich <klandwich@eff360d0-8e89-467c-86ba-6985a6ba51a3>
2013-01-03 08:39:12 +0000

    Commit is related to the following issues: 104

--- bin/sasunit.9.2_32.windows.de.overwrite.cmd
+++ bin/sasunit.9.2_32.windows.de.overwrite.cmd
@@ -11,5 +11,8 @@ if %ERRORLEVEL%==0 goto normalexit
 @echo. 
 @echo Exit code: %ERRORLEVEL%
 @echo. 
+if %ERRORLEVEL%==1 @echo SAS ended with warnings. Review run_all.log for details.
+if %ERRORLEVEL%==2 @echo SAS ended with errors! Check run_all.log for details!
+@echo. 
 pause
 :normalexit
