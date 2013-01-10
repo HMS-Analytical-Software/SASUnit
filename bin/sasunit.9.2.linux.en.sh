aee3900c3f399facb184a22085f9cefb090e0116
b-braun <b-braun@eff360d0-8e89-467c-86ba-6985a6ba51a3>
2013-01-10 11:48:38 +0000

    Extended error message for linux .sh files implemented + bugfix concerning SASUNIT_ROOT in non examples .sh files Commit is related to the following issues: 106

--- bin/sasunit.9.2.linux.en.sh
+++ bin/sasunit.9.2.linux.en.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 cd ..
-export SASUNIT_ROOT=$(pwd)
+export SASUNIT_ROOT=$(readlink -f .)
 export SASUNIT_OVERWRITE=0
 export SASUNIT_LANGUAGE=en
 export SASUNIT_HOST_OS=linux
 export SASUNIT_SAS_VERSION=9.2
 export SASCFGPATH=./bin/sasunit.$SASUNIT_SAS_VERSION.$SASUNIT_HOST_OS.$SASUNIT_LANGUAGE.cfg
-echo "Starting SASUnit in Overwrite Mode ..."
+echo "Starting SASUnit ..."
 /usr/local/SAS/SASFoundation/$SASUNIT_SAS_VERSION/bin/sas_$SASUNIT_LANGUAGE -nosyntaxcheck -noovp
-echo "SAS Exit Status: $?"          # Show SAS exit status
+
+# Show SAS exit status
+RETVAL=$?
+if [ $RETVAL -eq 1 ] 
+	then printf "\nSAS ended with warnings. Review run_all.log for details.\n"
+elif [ $RETVAL -eq 2 ] 
+	then printf "\nSAS ended with errors! Check run_all.log for details!\n"
+fi
+
 if [ -f  doc/sasunit/$SASUNIT_LANGUAGE/rep/index.html ]          
   then read -p "Would you like to see the results [y|n]? "
   case "$REPLY" in
--- example/bin/sasunit.9.2.linux.en.overwrite.sh
+++ example/bin/sasunit.9.2.linux.en.overwrite.sh
@@ -8,7 +8,15 @@ export SASUNIT_SAS_VERSION=9.2
 export SASCFGPATH=./bin/sasunit.$SASUNIT_SAS_VERSION.$SASUNIT_HOST_OS.$SASUNIT_LANGUAGE.cfg
 echo "Starting SASUnit Examples in Overwrite Mode ..."
 /usr/local/SAS/SASFoundation/$SASUNIT_SAS_VERSION/bin/sas_$SASUNIT_LANGUAGE -nosyntaxcheck -noovp
-echo "SAS Exit Status: $?"          # Show SAS exit status
+
+# Show SAS exit status
+RETVAL=$?
+if [ $RETVAL -eq 1 ] 
+	then printf "\nSAS ended with warnings. Review run_all.log for details.\n"
+elif [ $RETVAL -eq 2 ] 
+	then printf "\nSAS ended with errors! Check run_all.log for details!\n"
+fi
+
 if [ -f  doc/sasunit/$SASUNIT_LANGUAGE/rep/index.html ]          
   then read -p "Would you like to see the results [y|n]? "
   case "$REPLY" in
