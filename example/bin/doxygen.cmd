74f2dc400f18eaa3a3c845fde36359f9b1b29e98
klandwich <klandwich@eff360d0-8e89-467c-86ba-6985a6ba51a3>
2013-01-28 13:55:14 +0000

    Included check for exit code Commit is related to the following issues: 104

--- example/bin/doxygen.cmd
+++ example/bin/doxygen.cmd
@@ -6,4 +6,11 @@ rem         or https://sourceforge.net/p/sasunit/wiki/readme.v1.2/.
 @echo on
 
 "C:\Program Files\doxygen\bin\doxygen.exe" doxygen.cfg
+
+@echo off
+if %ERRORLEVEL%==0 goto normalexit
+@echo. 
+@echo Exit code: %ERRORLEVEL%
+@echo. 
 pause
+:normalexit
