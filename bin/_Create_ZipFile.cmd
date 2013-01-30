7788d07cc2d4b9f118afb8fadbc55981765b0424
klandwich <klandwich@eff360d0-8e89-467c-86ba-6985a6ba51a3>
2013-01-30 15:40:35 +0000

    New script file to create zip deliverable using 7-zip Commit is related to the following issues: 99

---
+++ bin/_Create_ZipFile.cmd
@@ -0,0 +1,24 @@
+REM @echo off
+
+cd \projects
+
+del SASUnit\SASUnit*.zip
+
+"C:\Program Files\7-Zip\7z.exe" a -tzip SASUnit\SASUnit_v1.2.zip SASUnit\Example\*
+"C:\Program Files\7-Zip\7z.exe" a -tzip SASUnit\SASUnit_v1.2.zip SASUnit\saspgm\sasunit\*
+"C:\Program Files\7-Zip\7z.exe" a -tzip SASUnit\SASUnit_v1.2.zip SASUnit\saspgm\template\*
+
+"C:\Program Files\7-Zip\7z.exe" t SASUnit\SASUnit_v1.2.zip -r
+
+
+if %ERRORLEVEL%==0 goto normalexit
+@echo. 
+@echo Exit code: %ERRORLEVEL%
+@echo. 
+if %ERRORLEVEL%==1 @echo SAS ended with warnings. Review run_all.log for details.
+if %ERRORLEVEL%==2 @echo SAS ended with errors! Check run_all.log for details!
+@echo. 
+pause
+:normalexit
+
+pause
