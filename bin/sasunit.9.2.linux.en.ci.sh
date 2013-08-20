2e80687e3eb942ee0a07a38b12c83bbde90ebab3
klandwich <klandwich@eff360d0-8e89-467c-86ba-6985a6ba51a3>
2013-08-20 03:35:46 +0000

    New CI Scripts for Linux without starting the browser at the end Commit is related to the following issues: 123

---
+++ bin/sasunit.9.2.linux.en.ci.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#						Copyright 2010, 2012 HMS Analytical Software GmbH.
+#           This file is part of SASUnit, the Unit testing framework for SAS(R) programs.
+#           For terms of usage under the GPL license see included file readme.txt
+#           or https://sourceforge.net/p/sasunit/wiki/readme.v1.2/.
+
+cd ..
+export SASUNIT_ROOT=$(readlink -f .)
+export SASUNIT_OVERWRITE=1
+export SASUNIT_COVERAGEASSESSMENT=0
+export SASUNIT_LANGUAGE=en
+export SASUNIT_HOST_OS=linux
+export SASUNIT_SAS_VERSION=9.2
+export SASCFGPATH=./bin/sasunit.$SASUNIT_SAS_VERSION.$SASUNIT_HOST_OS.$SASUNIT_LANGUAGE.cfg
+echo "Starting SASUnit in Overwrite Mode ..."
+/usr/local/SAS/SASFoundation/$SASUNIT_SAS_VERSION/bin/sas_$SASUNIT_LANGUAGE -nosyntaxcheck -noovp
+
+# Show SAS exit status
+RETVAL=$?
+if [ $RETVAL -eq 1 ] 
+	then printf "\nSAS ended with warnings. Review run_all.log for details.\n"
+elif [ $RETVAL -eq 2 ] 
+	then printf "\nSAS ended with errors! Check run_all.log for details!\n"
+fi
