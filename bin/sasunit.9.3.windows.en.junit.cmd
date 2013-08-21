705298ce8437da027d949ce4bcba6fd0840d2151
klandwich <klandwich@eff360d0-8e89-467c-86ba-6985a6ba51a3>
2013-08-21 09:36:59 +0000

    Renamed command files for junit integration Commit is related to the following issues: 123

--- bin/sasunit.9.3.windows.en.junit.cmd
+++ bin/sasunit.9.3.windows.en.junit.cmd
@@ -12,7 +12,7 @@ SET SASUNIT_LANGUAGE=en
 SET SASUNIT_HOST_OS=windows
 SET SASUNIT_SAS_VERSION=9.3
 SET SASUNIT_COVERAGEASSESSMENT=0
-"C:\Program Files\SASHome\SASFoundation\9.3\sas.exe" -CONFIG "bin\sasunit.%SASUNIT_SAS_VERSION%.%SASUNIT_HOST_OS%.%SASUNIT_LANGUAGE%_junit.cfg" -no$syntaxcheck -noovp -nosplash
+"C:\Program Files\SASHome\SASFoundation\9.3\sas.exe" -CONFIG "bin\sasunit.%SASUNIT_SAS_VERSION%.%SASUNIT_HOST_OS%.%SASUNIT_LANGUAGE%.junit.cfg" -no$syntaxcheck -noovp -nosplash
 
 @echo off
 if %ERRORLEVEL%==0 goto normalexit
