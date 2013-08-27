ee87826b90924147a6838da1d3e6d342d66c1e4c
klandwich <klandwich@eff360d0-8e89-467c-86ba-6985a6ba51a3>
2013-08-27 04:04:41 +0000

    Rearranged command scripts for self tests. Linux needs separate command files for junit, because we don't want out batch jobs to ask whether to start a browser or not. Commit is related to the following issues: 123

--- bin/sasunit.9.2.linux.en.ci.sh
+++ bin/sasunit.9.2.linux.en.ci.sh
@@ -12,7 +12,7 @@ export SASUNIT_LANGUAGE=en
 export SASUNIT_HOST_OS=linux
 export SASUNIT_SAS_VERSION=9.2
 export SASCFGPATH=./bin/sasunit.$SASUNIT_SAS_VERSION.$SASUNIT_HOST_OS.$SASUNIT_LANGUAGE.cfg
-echo "Starting SASUnit in Overwrite Mode ..."
+echo "Starting SASUnit ..."
 /usr/local/SAS/SASFoundation/$SASUNIT_SAS_VERSION/bin/sas_$SASUNIT_LANGUAGE -nosyntaxcheck -noovp
 
 # Show SAS exit status
