4434fe1a47f6366e94a7a52713ed0130489a6b68
klandwich <klandwich@eff360d0-8e89-467c-86ba-6985a6ba51a3>
2015-08-10 07:06:50 +0000

    Deleted references to doxygen Commit is related to the following issues: 55

--- example/bin/doxygen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#						Copyright 2010, 2012 HMS Analytical Software GmbH.
-#           This file is part of SASUnit, the Unit testing framework for SAS(R) programs.
-#           For terms of usage under the GPL license see included file readme.txt
-#           or https://sourceforge.net/p/sasunit/wiki/readme.v1.2/.
-
-# call documentation system doxygen 
-doxygen doxygen.cfg
