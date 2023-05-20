d6de8914cc79b8f1405543d7533c3b4f5480d253
Samuel Melm <samuel.melm@analytical-software.de>
2023-05-20 12:03:08 +0200

    Move back run number to param for more consistency

--- .github/scripts/backup_test_to_p_drive.sh
+++ .github/scripts/backup_test_to_p_drive.sh
@@ -4,12 +4,13 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
+run_number=$1
 language=$2
 target_name=${3:-linux_$language}
 
 P_DRIVE=/mnt/hms/laufwerke/p/hms_interne_projekte/00773_kompetenzfelder/00773-006_SAS/SASUnit/github
 timestamp=$(date "+%Y_%m_%d")
-path=$P_DRIVE/$timestamp/$RUN_NUMBER/$target_name
+path=$P_DRIVE/$timestamp/$run_number/$target_name
 
 kinit samuel.melm@ANALYTICAL-SOFTWARE.EU -k -t /home/github/samuel.melm.keytab
 
