6cbeec54854c383d7f396982b655b9b17bdd61bf
Samuel Melm <samuel.melm@analytical-software.de>
2023-07-11 17:24:41 +0200

    Experiment use old keytab file

--- .github/scripts/copy_to_p_drive.sh
+++ .github/scripts/copy_to_p_drive.sh
@@ -13,12 +13,14 @@ fi
 src=$1
 tgt_dir=$2
 
-set p_drive="//analytical-software.eu/projekte/hms_interne_projekte/00773_kompetenzfelder/00773-006_SAS/SASUnit/github"
 p_drive=/mnt/hms/laufwerke/p/hms_interne_projekte/00773_kompetenzfelder/00773-006_SAS/SASUnit/github
 timestamp=$(date "+%Y_%m_%d")
 path=$p_drive/$timestamp/$RUN_NUMBER/$tgt_dir
 
-kinit svc-00773-006-github@ANALYTICAL-SOFTWARE.EU -k -t /home/github/svc-00773-006-github.keytab
+# kinit svc-00773-006-github@ANALYTICAL-SOFTWARE.EU -k -t /home/github/svc-00773-006-github.keytab
+
+kinit samuel.melm@ANALYTICAL-SOFTWARE.EU -k -t /home/github/sme.keytab
+
 
 mkdir -p $path
 
