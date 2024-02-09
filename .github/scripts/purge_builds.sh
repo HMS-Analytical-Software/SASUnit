018da703f4696de4d9f60ba066fa499c58910fd6
KLandwich <klaus.landwich@analytical-software.de>
2024-02-09 16:56:39 +0100

    Update script and rename workflow

--- .github/scripts/purge_builds.sh
+++ .github/scripts/purge_builds.sh
@@ -4,27 +4,22 @@ if [ $# -lt 1 ]; then
 fi
 exit
 
+echo ""
+echo "----------------------------------------------"
+
 p_drive=$1
 echo "--- p_drive:" $p_drive
-exit
 
 echo "Checking files"
 files=$(find -P $p_drive/ -maxdepth 2 -mindepth 2 -type d)
-echo "--- files:" $files
-
 filecount=$(echo "$files" | wc -l)
-echo "--- filecount:" $filecount
+((filescount2del=$filecount - ${{ env.RUNS_TO_KEEP }}))
 
-echo ""
-echo "----------------------------------------------"
-echo "found $filecount will be compare with ${{ env.RUNS_TO_KEEP }}"
-
-if [ $filecount -le ${{ env.RUNS_TO_KEEP }} ]; then
-echo "Nothing to do"
-exit 0
+if [ $filescount2del -le 0 ]; then
+  echo "Nothing to do"
+  exit 0
 fi
 
-((filescount2del=$filecount - ${{ env.RUNS_TO_KEEP }}))
 files2del=$(echo "$files" | sort | head -n $filescount2del)
 
 echo "found $filecount will delete $filescount2del"
